44bde1fb6ee3f8a7e130005e94b99b4bdc786ee5
jcaylao <122569462+jcaylao@users.noreply.github.com>
2025-04-23 16:20:26 -0700

    Created pages and css styling file

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hello! I'm a passionate web developer.</p>
+        </div>
+    );
+};
+
+export default Home;
